c75ca641a49b741f556958785c46c2fbfa90ee15
jhonnatas <jhonnatas.aires@gmail.com>
2019-01-26 16:50:16 -0300

    Added image to movies and some layout improvements

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,8 @@ class Movie < ApplicationRecord
 
 	belongs_to :genre
 
+	has_one_attached :img #Active Storage
+
 	validates :title, :title_br, :tipo, presence: true
 	validates :title, uniqueness: true
 end
